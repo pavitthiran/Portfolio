6280f80a922efb6d1269e9f797463e83f6a79a4b
User <user@example.com>
2026-04-09 15:40:59 +0530

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
